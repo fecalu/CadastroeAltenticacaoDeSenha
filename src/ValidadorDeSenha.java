a058f512772f2d9d10c131bb4f6ace040a0bb266
Joao Felipe <joaofelipe.camposs@gmail.com>
2024-09-23 19:57:31 -0300

    Criacao do metodo para validar senha

--- src/ValidadorDeSenha.java
+++ src/ValidadorDeSenha.java
@@ -1,4 +1,34 @@
 
 public class ValidadorDeSenha {
-
+	
+	
+		public static boolean validadorLetras(String senha) {
+			
+			Boolean temMaiuscula = null;
+			Boolean temMinuscula = null;
+			Boolean temCaracter = null;
+			
+			if (senha.chars().anyMatch(Character::isUpperCase) && senha.chars().anyMatch(Character::isLowerCase)) {
+				temMaiuscula = true;
+				temMinuscula = true;
+			} else {
+				temMaiuscula = false;
+				temMinuscula = false;
+			}
+			
+			String[] caracteres = {"!", "@", "#", "$", "%", "&", "*", "(", ")", "-", "_"};
+			
+			for (int i = 0; i < caracteres.length; i++) {
+				if (senha.contains(caracteres[i])) {
+					temCaracter = true;
+				} 
+			}
+			
+			
+			return temMaiuscula && temMinuscula && temCaracter ;
+		}
+	
+	
+	
+	
 }
