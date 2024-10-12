a178cfae8dbc68dc9f8c11d6825990dd0547c7cd
Joao Felipe <joaofelipe.camposs@gmail.com>
2024-10-12 18:24:19 -0300

    atualizacao para poder mudar a senha

--- src/Programa.java
+++ src/Programa.java
@@ -3,62 +3,109 @@ import java.util.Scanner;
 
 public class Programa {
 
-	public static void main(String[] args) {
-		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		String senhaDeConfirmacao = null;
-		String senha = null;
-		
-		
-		System.out.println("Digite sua senha: ");
-		senha = sc.nextLine();
-		
-	
-		
-		
-		
-        if (ValidadorDeSenha.validadorLetras(senha)) {
-            System.out.println("Senha válida."); 
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String senha = null;
+        String senhaDeConfirmacao = null;
+
+        // Solicita a senha inicial
+        System.out.println("Digite sua senha: ");
+        senha = sc.nextLine();
+
+        // Valida se a senha contém letras usando o validador
+        if (!ValidadorDeSenha.validadorLetras(senha)) {
+            System.out.println("Senha inválida. A senha deve conter letras.");
+            return;
         }
-		
-        System.out.println("Confirme sua Senha");
+
+        // Confirmação da senha
+        System.out.println("Confirme sua senha: ");
         senhaDeConfirmacao = sc.nextLine();
-        
-        if (senha.equals(senhaDeConfirmacao)) {
-			System.out.println("Senha criada com sucesso!");
-		}
-        else {
-        	int cont = 0;
-        	while (!(senha.equals(senhaDeConfirmacao)) && cont < 3) {
-				System.out.println("Senhas diferentes, digite novamente: " + (cont + 1) +"/3");
-				senhaDeConfirmacao = sc.nextLine();
-				cont++;
-			}
-        	if (senha.equals(senhaDeConfirmacao)) {
-        		System.out.println("Senha criada com sucesso!");
-			}else {
-				System.out.println("Senha bloqueada!");
-			}
-        	
+
+        // Verifica se as senhas são iguais
+        if (!senha.equals(senhaDeConfirmacao)) {
+            if (!repetirConfirmacao(senha, sc, 3)) {
+                System.out.println("Tentativas excedidas. Senha bloqueada!");
+                return;
+            }
         }
 
+        System.out.println("Senha criada com sucesso!");
 
-		
-		System.out.println("Testifica");
-		
-		
-		
-		
-		
-		
-		
-		
-		sc.close();
-		
-		
+        // Simulação de hackeamento e alteração de senha
+        System.out.println("Você foi hackeado, mude sua senha URGENTE!");
 
-	}
+        if (!alterarSenha(senha, sc)) {
+            System.out.println("Tentativas excedidas. Senha bloqueada!");
+        } else {
+            System.out.println("Senha alterada com sucesso!");
+        }
 
+        sc.close();
+    }
+
+    // Método para confirmar a senha com limite de tentativas
+    public static boolean repetirConfirmacao(String senha, Scanner sc, int tentativasMaximas) {
+        int cont = 0;
+        while (cont < tentativasMaximas) {
+            System.out.println("Senhas diferentes, digite novamente: " + (cont + 1) + "/" + tentativasMaximas);
+            String senhaDeConfirmacao = sc.nextLine();
+            if (senha.equals(senhaDeConfirmacao)) {
+                return true;
+            }
+            cont++;
+        }
+        return false;
+    }
+
+    // Método para alterar a senha com validação e limite de tentativas
+    public static boolean alterarSenha(String senhaAtual, Scanner sc) {
+        System.out.println("Digite sua senha atual: ");
+        String senha = sc.nextLine();
+        int cont = 0;
+
+        while (!senha.equals(senhaAtual) && cont < 3) {
+            System.out.println("Senha incorreta. Digite novamente: " + (cont + 1) + "/3");
+            senha = sc.nextLine();
+            cont++;
+        }
+
+        if (!senha.equals(senhaAtual)) {
+            return false;
+        }
+
+        // Nova senha com limite de 3 tentativas
+        String novaSenha = null;
+        cont = 0;
+        boolean senhaValida = false;
+        while (cont < 3 && !senhaValida) {
+            System.out.println("Digite sua nova senha: ");
+            novaSenha = sc.nextLine();
+
+            // Validação da nova senha
+            if (ValidadorDeSenha.validadorLetras(novaSenha)) {
+                senhaValida = true;
+            } else {
+                System.out.println("Nova senha inválida. A senha deve conter letras. Tentativa " + (cont + 1) + "/3");
+                cont++;
+            }
+        }
+
+        if (!senhaValida) {
+            return false;
+        }
+
+        // Confirmação da nova senha
+        System.out.println("Confirme sua nova senha: ");
+        String confirmacaoNovaSenha = sc.nextLine();
+
+        if (!novaSenha.equals(confirmacaoNovaSenha)) {
+            return repetirConfirmacao(novaSenha, sc, 3);
+        }
+
+        return true;
+    }
 }
